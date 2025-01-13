d186d8a796ce1470248d89a2e98d1733f35cb66e
Hannu Packalen <hpackalen@lcs.on.ca>
2025-01-13 09:07:49 -0500

    Initial Commit

---
+++ CalculatingApp/CalculatingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatingAppApp.swift
+//  CalculatingApp
+//
+//  Created by Hannu Packalen on 2025-01-13.
+//
+
+import SwiftUI
+
+@main
+struct CalculatingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
